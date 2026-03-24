c001ee5e529ac411038b1874fee044698bc4ba8e
meathoo <john489142@gmail.com>
2026-03-24 23:11:32 +0800

    setup github pages deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue_evaluate/' : '/',
   configureWebpack: {
     // Set up all the aliases we use in our app.
     plugins: [
